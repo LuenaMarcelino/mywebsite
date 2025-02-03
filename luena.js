214be2b82405ac2f6c206deab679edaff1a7e384
LuenaMarcelino <luena.marcelino@gmail.com>
2025-02-03 21:26:41 +0200

    Add files via upload

--- luena.js
+++ luena.js
@@ -1,16 +1,4 @@
-
-const newImages = [
-    'images/image.png', 
-    'images/image2.png', 
-    'images/image5.png'
-]
-
-// const images = document.querySelectorAll(".image");
-// const button = document.getElementById('change-images');
-// console.log(images)
-
-    
-    images.forEach((image, index)=>{
+images.forEach((image, index)=>{
         if(image.classList.contains('fade-out')){
             image.classList.add('fade-out')
         }else if (image.classList.contains('blackout')){
@@ -25,4 +13,40 @@ const newImages = [
         }, 1000)
     })
 
+    function checkService() {
+        const allowedService = ["Hair Wash", "Hair Styling", "Hair Dye"];
+        let selectedService = document.getElementById("Service").value.trim();
+        if (selectedService.includes === "") {
+            document.getElementById("message").textContent = "Select a service";
+            document.getElementById("message").style.color = 'green';
+            return;
+        }
+        if (allowedService.includes(selectedService)) {
+            document.getElementById("message").textContent = "service booked!";
+            document.getElementById("message").style.color = 'green';
+        }
+         else {
+            document.getElementById("message").textContent = "Sorry, our salon doesn't provide this service.";
+            document.getElementById("message").style.color = 'red';
+        }
+    }
 
+
+    const apiKey = '2EZDjj7WtT50J23fR0ZLATAlCKamN4Li'; // My Giphy API key
+
+    $(document).ready(function() {
+        $('#get-gif').click(function() {
+            fetchRandomGif();
+        });
+    });
+    
+    function fetchRandomGif() {
+        const url = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`;
+    
+        $.get(url, function(data) {
+            const gifUrl = data.data.images.original.url;
+            $('#gif-container').html(`<img src="${gifUrl}" alt="Random GIF">`);
+        }).fail(function() {
+            $('#gif-container').html('<p>Error fetching GIF. Please try again.</p>');
+        });
+    }
